66cbdf5dfb6a24f4f2f9be069e8713dbe2a9729c
sumeet <sumeetg2k11@hotmail.com>
2022-10-18 14:08:54 +1300

    Update App.test.js

--- App.test.js
+++ App.test.js
@@ -13,7 +13,6 @@ const cart = ["A", "B", "C", "D"];
 const cart2 = ["D", "A", "A", "A", "D", "B", "C"];
 const cart3 = ["A", "A", "D", "B", "C"];
 const cart4 = ["D", "A", "A", "A", "D", "C", "C"];
-const cart5 = ["A", "A", "A", "D", "B", "B"]; //135 + 45 + 15
 
 const specials = [
   { type: "A", amount: 3, special: 135 },
@@ -26,5 +25,4 @@ test("Checkout calcultor test", () => {
   expect(checkoutCalculator(priceList, cart2, specials)).toBe(215);
   expect(checkoutCalculator(priceList, cart3, specials)).toBe(165);
   expect(checkoutCalculator(priceList, cart4, specials)).toBe(205);
-  expect(checkoutCalculator(priceList, cart5, specials)).toBe(205);
 });
